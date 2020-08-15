b60d40de60ad4bac967e050af6a1ade260e07a8f
sunzhichaoaa <95872680@qq.com>
2020-08-15 17:32:23 +0800

    [SfWeather][创建数据库实体类]

---
+++ app/src/main/java/com/example/sfweather/db/Country.java
@@ -0,0 +1,42 @@
+package com.example.sfweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Country extends DataSupport {
+    private int id;
+    private String contyName;
+    private String WeatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContyName() {
+        return contyName;
+    }
+
+    public void setContyName(String contyName) {
+        this.contyName = contyName;
+    }
+
+    public String getWeatherId() {
+        return WeatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        WeatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
